9fe5c71c32c003dc65dfe76dcd606489099dd879
vijaykrishna.pro <vijaykrishna.programmer@gmail.com>
2022-10-25 13:22:02 +0530

    First

---
+++ script.js
@@ -0,0 +1,49 @@
+// import saveAs from 'file-saver/src/FileSaver';
+
+
+let rawTextEl = document.getElementById("rawText");
+
+let rawtextSubmitEl = document.getElementById("rawtextSubmit");
+
+let displayListEl = document.getElementById("displayList");
+
+let paraEl = document.getElementById("para");
+
+
+let mylist = [];
+function processRawTextAndPasteInDisplay() {
+
+  let textofuser = rawTextEl.value;
+  textofuser = textofuser.replace(/[^a-zA-Z ]/g, " ");
+  // console.log(textofuser);
+  textofuser = textofuser.split(" ");
+  // console.log(textofuser);
+  textofuser = textofuser.map(element => {
+    return element.toLowerCase();
+  });
+  // textofuser = textofuser.sort((a, b) => b.length - a.length);
+  textofuser = textofuser.sort();
+  textofuser = [...new Set(textofuser)];
+  // let count = 0;
+
+  for (let a of textofuser) {
+    mylist.push(a);
+  }
+
+  let data_list = JSON.stringify(mylist);
+
+  displayListEl.value = data_list;
+
+  // var blob = new Blob([data_list], {type: "text/plain;charset=utf-8"});
+  // saveAs(blob, "sample.txt");
+
+
+}
+
+rawtextSubmitEl.onclick = function() {
+  processRawTextAndPasteInDisplay();
+
+  
+
+
+}
